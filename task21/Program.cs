154b23e020e0ee93dfc9f815f70ca64158ba9e17
Наталья Сибгатулина <mehryak@mail.ru>
2023-04-02 01:49:06 +0500

    add task19,21,23

---
+++ task21/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 21
+Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+int Read(string argument)
+ {
+int number;
+System.Console.WriteLine($"inpup {argument}: ");
+while((!int.TryParse(Console.ReadLine(), out number)))
+{
+System.Console.WriteLine("Not a number");
+}
+return number;
+} 
+
+double Legth()
+{
+int x1 = Read("x1");
+int y1 = Read("y1");
+int z1 = Read("z1");
+int x2 = Read("x2");
+int y2 = Read("y2");
+int z2 = Read("z2");
+double Length = Math.Round(Math.Sqrt(Math.Pow( x1 - x2, 2 ) + Math.Pow( y1 - y2, 2 )+ Math.Pow( z1 - z2, 2)),2);
+return Length;
+}
+
+void Answer()
+{
+double L = Legth();
+System.Console.WriteLine($"Расстояние между точками  = {L}");
+
+}
+
+Answer();
---
+++ task23/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+    tableCube(number);
+}
+else
+    Console.WriteLine($"Введено некорректное число");
+
+void tableCube(int num)
+{
+    int i = 1;
+    while (i <= num)
+    {
+        double Cube = Math.Pow(i,3);
+
+        Console.WriteLine($"{i,3} ->  {i * i * i,3}");
+        i++;
+    }
+
+}
+
+/* Line();
+
+int Read(string argument)
+{
+int number;
+System.Console.WriteLine($"inpup {argument}: ");
+while((!int.TryParse(Console.ReadLine(), out number)) || number < 0)
+{
+System.Console.WriteLine("Not a number");
+}
+return number;
+}
+
+void Line()
+{
+int N = Read("N");
+for(int i = 1; i <= N; i++)
+{
+double Cube = Math.Pow(i,3);
+System.Console.Write($" {Cube};");
+}
+} */
